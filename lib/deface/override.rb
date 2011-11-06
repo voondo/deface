de7396e69d33f1b4d12ed528843d53542e8418f9
Ryan Bigg <radarlistener@gmail.com>
2011-11-06 14:52:52 +0200

    Remove beginning slash prefix from virtual path using gsub

--- lib/deface/override.rb
+++ lib/deface/override.rb
@@ -336,7 +336,7 @@ module Deface
       virtual_path = details[:virtual_path]
       return [] if virtual_path.nil?
 
-      virtual_path = virtual_path[1..-1] if virtual_path.first == '/'
+      virtual_path = virtual_path.gsub(/^\//, '')
 
       result = []
       result << self.all[virtual_path.to_sym].try(:values)
