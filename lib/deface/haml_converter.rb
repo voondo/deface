0774e6a315b72b0bf7d883d3fd0a960f4f37ccb4
Brian Quinn <briandquinn@gmail.com>
2012-01-23 15:56:37 +0000

    Added attribute parsing for haml to erb conversion

--- lib/deface/haml_converter.rb
+++ lib/deface/haml_converter.rb
@@ -8,5 +8,50 @@ module Deface
     def push_silent(text, can_suppress = false)
       push_text "<% #{text.strip} %>"
     end
+
+    def parse_old_attributes(line)
+      attributes_hash, rest, last_line = super(line)
+
+      attributes_hash = deface_attributes(attributes_hash)
+
+      return attributes_hash, rest, last_line
+    end
+
+
+    def parse_new_attributes(line)
+      attributes, rest, last_line = super(line)
+
+      attributes[1] = deface_attributes(attributes[1])
+
+      return attributes, rest, last_line
+    end
+
+    private
+
+      # coverts { attributes into deface compatibily attributes
+      def deface_attributes(attrs)
+        return if attrs.nil?
+        attrs.gsub! /\{|\}/, ''
+        attrs = attrs.split(',')
+
+        debugger
+        if attrs.join.include? "=>"
+          attrs.map!{|a| a.split("=>").map(&:strip) }
+        else
+          attrs.map!{|a| a.split(": ").map(&:strip) }
+        end
+
+        attrs.map! do |a|
+          if a[1][0] != ?' && a[1][0] != ?"
+            a[0] = %Q{"data-erb-#{a[0].gsub(/:|'|"/,'')}"}
+            a[1] = %Q{"<%= #{a[1]} %>"}
+          end
+
+          a
+        end
+
+        attrs.map{ |a| a.join " => " }.join(', ')
+      end
+
   end
 end
