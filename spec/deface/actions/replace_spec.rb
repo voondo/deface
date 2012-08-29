193013e8717f5a365d51d4d52c9f8dfc211ad07e
Brian Quinn <briandquinn@gmail.com>
2012-08-29 14:26:57 +0100

    Improve spec coverage for bad selectors

--- spec/deface/actions/replace_spec.rb
+++ spec/deface/actions/replace_spec.rb
@@ -23,6 +23,26 @@ module Deface
           Dummy.apply(source, {:virtual_path => "posts/index"}).should == "<span>Argh!</span>"
         end
       end
+
+      describe "with a single replace override with bad closing_selector defined" do
+        before { Deface::Override.new(:virtual_path => "posts/index", :name => "Posts#index", :replace => "h1", :closing_selector => "h3", :text => "<span>Argh!</span>") }
+        let(:source) { "<h1>start</h1><p>some junk</p><div>more junk</div><h2>end</h2>" }
+
+        it "should log error and return unmodified source" do
+          Rails.logger.should_receive(:info).with(/failed to match with end selector/)
+          Dummy.apply(source, {:virtual_path => "posts/index"}).should == source
+        end
+      end
+
+      describe "with a single replace override with bad selector defined" do
+        before { Deface::Override.new(:virtual_path => "posts/index", :name => "Posts#index", :replace => "h3", :closing_selector => "h2", :text => "<span>Argh!</span>") }
+        let(:source) { "<h1>start</h1><p>some junk</p><div>more junk</div><h2>end</h2>" }
+
+        it "should log error and return unmodified source" do
+          Rails.logger.should_receive(:info).with(/failed to match with starting selector/)
+          Dummy.apply(source, {:virtual_path => "posts/index"}).should == source
+        end
+      end
     end
   end
 end
