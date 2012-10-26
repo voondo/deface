142efd77970d07b23addff2ca04ae33c9eb6b11c
Jens Balvig <jens@balvig.com>
2012-10-26 19:23:07 +0900

    Use examples from docs

--- spec/generators/deface/override_generator_spec.rb
+++ spec/generators/deface/override_generator_spec.rb
@@ -3,23 +3,23 @@ require 'generator_spec/test_case'
 
 describe Deface::Generators::OverrideGenerator do
   include GeneratorSpec::TestCase
-  destination File.expand_path("../../tmp", __FILE__)
+  destination File.expand_path("../../../tmp", __FILE__)
 
-  before do
+  before(:all) do
     prepare_destination
   end
 
   context 'using erb' do
     it "should generate a deface override with the correct path" do
       run_generator %w(posts/_post add_headline)
-      assert_file 'app/overrides/posts/_post/add_headline.html.erb.deface', "<!-- insert_top 'body' -->\n<h2>These robots are awesome.</h2>\n"
+      assert_file 'app/overrides/posts/_post/add_headline.html.erb.deface', "<!-- insert_after 'h1' -->\n<h2>These robots are awesome.</h2>\n"
     end
   end
 
   context 'using haml' do
     it "should generate a deface override with the correct path" do
       run_generator %w(posts/_post add_headline -e haml)
-      assert_file 'app/overrides/posts/_post/add_headline.html.haml.deface', "/\n  insert_top 'body'\n%h2 These robots are awesome.\n"
+      assert_file 'app/overrides/posts/_post/add_headline.html.haml.deface', "/\n  insert_after 'h1'\n%h2 These robots are awesome.\n"
     end
   end
 
